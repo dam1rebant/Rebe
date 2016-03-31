144e69d98e6daf6f65548bc54a1873578a38b433
Rebee <Rebee@Rebe>
2016-03-31 09:54:05 +0200

    Prueba Git

---
+++ Github/src/github/Github.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+/**
+ *
+ * @author Rebee
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=2;
+        int b=5;
+        String c = "edu";
+        String d = "rebe";
+        
+        System.out.println(c+" va a sacar un "+b+" y "+d+" va a sacar un "+a);
+    }
+    
+}
